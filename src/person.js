62279a065bfbf56f1ab08ac710afafc058114844
Manikanta L.S.E <manikanta.lse@gmail.com>
2019-01-20 21:50:49 +0530

    Add files via upload

---
+++ src/person.js
@@ -0,0 +1,81 @@
+/* eslint-disable */
+export const PERSON = {
+  name: {
+    first: 'Maddala',
+    middle: 'L S E',
+    last: 'Manikanta',
+  },
+  about: 'I\'m M. Manikanta L.S.E., a self taught web deveopler from Hyderabad, India. I have 1 year of industrial experience in both front-end and back-end development. I am passionate about creating Happy Digital Interactions for people. I\'m currently available for projects small or large.',
+  position: 'Software Engineer',
+  birth: {
+    year: 1995,
+    location: 'Andhra Pradesh'
+  },
+  experience: [
+    {
+      company: 'GGK Tech',
+      position: 'Software Engineer',
+      timeperiod: 'Since June 2018',
+      description: 'Worked as full stack web developer on projects based on PHP and it\'s MVC frameworks such as Yii2 and Joomla. I have also worked on projects using Angular/React and Node.js'
+    },  
+    {
+      company: 'Freelance',
+      position: 'Web Developer',
+      timeperiod: 'September 2017 - May 2018',
+      description: 'Worked on few freelance projects using PHP which include developing web applications, writing wordpress themes and plugins etc.,'
+    },            
+    {
+      company: 'Osmosys software solutions',
+      position: 'Associate Software Engineer',
+      timeperiod: 'September 2016 - August 2017',
+      description: 'Worked as full stack web developer on projects based on PHP and it\'s MVC frameworks such as CakePHP and writing wordpress themes and plugin development. Find out more here: https://manikantalse.github.io/'
+    }
+  ],
+  education: [{
+      degree: 'Bachelor of Technology',
+      timeperiod: 'September 2012 - May 2016',
+      description: 'Computer science degree from Sreyas Institute of Engineering and Technology(JNTUH), Hyderabad, Telangana[2012-2016].'
+    }
+  ],
+  // skill level goes 0 to 100
+  skills: [{
+      name: 'HTML5',
+      level: '90'
+    },
+    {
+      name: 'CSS3',
+      level: '90'
+    },
+    {
+      name: 'JavaScript',
+      level: '85'
+    },
+    {
+      name: 'PHP',
+      level: '90'
+    },
+    {
+      name: 'Angular',
+      level: '80'
+    },
+    {
+      name: 'React',
+      level: '75'
+    },        
+    {
+      name: 'Node.js',
+      level: '75'
+    },
+
+  ],
+  skillDescription: 'Portfolio website: https://manikantalse.github.io/',
+  contact: {
+    email: 'manikanta.lse@gmail.com',
+    phone: '9177608118',
+    street: 'Bhulaxmi nagar, Vanasthalipuram',
+    city: 'Hyderabad',
+    website: 'https://goo.gl/uSJ62B',
+    github: 'manikantalse'
+  },
+  lang: 'en' // en, de, fr, pt, cn, it, es, th, pt-br, ru, sv, id, hu
+};
